b4370c6884d53d708865172d9d4d63462cf97250
Zhdanov Viktor <36110525+NoManEXE@users.noreply.github.com>
2018-11-17 10:12:44 +0300

    weather api added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import Info from "./components/info";
 import Form from "./components/form"
 import Weather from "./components/Weather"
 
+const API_KEY = "32b14e59fd8ace4b46c72b4f58acda8e"
+
 class App extends React.Component {
+
+gettingWeather = async () => {
+   const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric`);
+   const data = await api_url.json();
+   console.log(data);
+  }
+
   render (){
     return (
       <div>
